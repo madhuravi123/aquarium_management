5496414bb3cf2cff22f20dbb20a2a69d24207a1f
Madhumitha Ravichandran <madhumitharavichandran22@gmail.com>
2026-02-17 23:51:44 +0530

    Updated aquarium management project

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar" style="width: 280px;">
     <a href="dashboard.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <span class="fs-4">AquaSys</span>
+        <span class="fs-4">Aquamart</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
